8ce370833eaca3ba8706b451ad2e1a3d2db14ee0
jgardner04 <jonathan.gardner04@gmail.com>
2017-07-03 16:32:22 -0500

    Removed uncessary shared files

--- EmailForm/run.csx.cs
+++
@@ -1,8 +0,0 @@
-using System.Net;
-
-public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
-{
-    return name == null
-        ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the reqest body")
-        : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
-}
--- Shared/Settings.csx
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class Settings 
-{
-	public static string GetSetting(string settingName)
-	{
-        return System.Environment.GetEnvironmentVariable(settingName, EnvironmentVariableTarget.Process);
-	}
-}
